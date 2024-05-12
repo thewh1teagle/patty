b207be14ca0162c715d49f9c36c7c5b9122008f3
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-12 04:53:33 +0300

    windows only example

--- examples/win_admin.rs
+++ examples/win_admin.rs
@@ -1,9 +1,13 @@
-use patty::{home_dir, Options, PathManager, RegistryKind};
+use patty::{home_dir, Options, PathManager};
+
+#[cfg(windows)]
+use patty::RegistryKind;
 
 fn main() {
     let home = home_dir().unwrap();
     let bin_path = home.join("bin");
     let options = Options {
+        #[cfg(windows)]
         kind: RegistryKind::System, // Require admin rights
         ..Default::default()
     };
